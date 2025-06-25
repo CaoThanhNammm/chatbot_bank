b8fa733d764aa230b9d3e850b1210b73ea9d5e3c
CaoThanhNammm <21130448@st.hcmuaf.edu.vn>
2025-06-25 09:10:00 +0700

    server on

--- UI/src/config/ApiUrlManager.js
+++ UI/src/config/ApiUrlManager.js
@@ -6,7 +6,7 @@
 class ApiUrlManager {
   constructor() {
     // Base URLs
-    this.NGROK_BASE = 'https://44ef-35-223-118-100.ngrok-free.app/api';
+    this.NGROK_BASE = 'https://cd4d-171-247-78-59.ngrok-free.app/api';
     this.NGROK_BASE_BE = 'https://2bf0-171-247-78-59.ngrok-free.app/api';
     
     // Common headers for ngrok requests - Only safe headers to avoid CORS preflight
