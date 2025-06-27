6360d7961b9d7eae4012dcfc022b02320c4d4ae9
CaoThanhNammm <21130448@st.hcmuaf.edu.vn>
2025-06-27 11:49:14 +0700

    ádas

--- UI/src/config/ApiUrlManager.js
+++ UI/src/config/ApiUrlManager.js
@@ -6,7 +6,7 @@
 class ApiUrlManager {
   constructor() {
     // Base URLs
-    this.NGROK_BASE = 'https://40cb-34-27-40-188.ngrok-free.app/api';
+    this.NGROK_BASE = 'https://50a0-34-27-40-188.ngrok-free.app/api';
     this.NGROK_BASE_BE = 'https://5b22-171-247-78-59.ngrok-free.app/api';
     
     // Common headers for ngrok requests
